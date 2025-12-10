4160120da842d29d04f898b7c614294070020750
Zaky <sultanzaky070310@gmail.com>
2025-12-10 19:51:35 +0700

    Rapiin kode dikit

--- CM_Beasiswa.java
+++ CM_Beasiswa.java
@@ -144,92 +144,92 @@ public class CM_Beasiswa {
         System.out.println();
     }
 
-        static void cariPendaftarBeasiswa() {
-            System.out.print("\nMasukkan jenis beasiswa yang dicari (Reguler/Unggulan/Riset): ");
-            String jenis = sc.nextLine().trim();
+    static void cariPendaftarBeasiswa() {
+        System.out.print("\nMasukkan jenis beasiswa yang dicari (Reguler/Unggulan/Riset): ");
+        String jenis = sc.nextLine().trim();
 
-            jenis = jenis.substring(0,1).toUpperCase() + jenis.substring(1).toLowerCase();
+        jenis = jenis.substring(0,1).toUpperCase() + jenis.substring(1).toLowerCase();
 
-            System.out.println("\n=== Hasil Pencarian Pendaftar Beasiswa ===");
-            System.out.printf("%-3s %-20s %-12s %-6s %-12s %-15s\n", "No","Nama","NIM", "IPK", "Beasiswa", "Penghasilan" );
+        System.out.println("\n=== Hasil Pencarian Pendaftar Beasiswa ===");
+        System.out.printf("%-3s %-20s %-12s %-6s %-12s %-15s\n", "No","Nama","NIM", "IPK", "Beasiswa", "Penghasilan" );
 
-            int nomor = 1;
-            boolean ditemukan = false;
+        int nomor = 1;
+        boolean ditemukan = false;
 
-            for (int i = 0; i < data.length; i++){
-                if (data[i][3].equalsIgnoreCase(jenis)){
-                    ditemukan = true;
-                    System.out.printf("%-3d ", nomor++);
+        for (int i = 0; i < data.length; i++){
+            if (data[i][3].equalsIgnoreCase(jenis)){
+                ditemukan = true;
+                System.out.printf("%-3d ", nomor++);
 
-                    for (int j = 0; j < 5; j++){
-                        if (j == 0){
-                            System.out.printf("%-20s ", data[i][j]);
-                        }else if (j == 1) {
-                            System.out.printf("%-12s ", data[i][j]);
-                        }else if (j == 2) {
-                            System.out.printf("%-6s ", data[i][j]);
-                        }else if (j == 3) {
-                            System.out.printf("%-12s ", data[i][j]);
-                        }else if (j == 4) {
-                            System.out.printf("%-15s ", data[i][j]);
-                        }
+                for (int j = 0; j < 5; j++){
+                    if (j == 0){
+                        System.out.printf("%-20s ", data[i][j]);
+                    }else if (j == 1) {
+                        System.out.printf("%-12s ", data[i][j]);
+                    }else if (j == 2) {
+                        System.out.printf("%-6s ", data[i][j]);
+                    }else if (j == 3) {
+                        System.out.printf("%-12s ", data[i][j]);
+                    }else if (j == 4) {
+                        System.out.printf("%-15s ", data[i][j]);
                     }
-                    System.out.println();
                 }
+                System.out.println();
             }
-            if (!ditemukan){
-                System.out.println("Tidak ada pendaftar dengan beasiswa " + jenis);
+        }
+        if (!ditemukan){
+            System.out.println("Tidak ada pendaftar dengan beasiswa " + jenis);
+        }
+    }
+
+    static void hitungRataIPK() {
+        if (data.length == 0) {
+            System.out.println("\nBelum ada pendaftar!");
+        }
+
+        double totalReguler =0, totalUnggulan = 0, totalRiset = 0;
+        int countReguler = 0, countUnggulan = 0, countRiset = 0;
+
+        for (int i=0; i < data.length; i++){
+            String jenis = data[i][3];
+            double ipk = Double.parseDouble(data[i][2]);
+
+            if (jenis.equalsIgnoreCase("Reguler")){
+                totalReguler += ipk;
+                countReguler++;
+            }else if (jenis.equalsIgnoreCase("Unggulan")){
+                totalUnggulan += ipk;
+                countUnggulan++;
+            }else if (jenis.equalsIgnoreCase("Riset")){
+                totalRiset += ipk;
+                countRiset++;
             }
         }
 
-        static void hitungRataIPK() {
-            if (data.length == 0) {
-                System.out.println("\nBelum ada pendaftar!");
-            }
+        System.out.println("\n=== Rata-rata IPK per Jenis Beasiswa ===");
 
-            double totalReguler =0, totalUnggulan = 0, totalRiset = 0;
-            int countReguler = 0, countUnggulan = 0, countRiset = 0;
-
-            for (int i=0; i < data.length; i++){
-                String jenis = data[i][3];
-                double ipk = Double.parseDouble(data[i][2]);
-
-                if (jenis.equalsIgnoreCase("Reguler")){
-                    totalReguler += ipk;
-                    countReguler++;
-                }else if (jenis.equalsIgnoreCase("Unggulan")){
-                    totalUnggulan += ipk;
-                    countUnggulan++;
-                }else if (jenis.equalsIgnoreCase("Riset")){
-                    totalRiset += ipk;
-                    countRiset++;
-                }
-            }
-
-            System.out.println("\n=== Rata-rata IPK per Jenis Beasiswa ===");
-
-            if (countReguler > 0){
-                double rataReguler = totalReguler/countReguler;
-                System.out.printf("Reguler  : rata-rata IPK = %.2f\n", rataReguler);
-            }else {
-                System.out.println("Reguler  : tidak ada pendaftar");
-            }
-
-            if (countUnggulan > 0) {
-                double rataUnggulan = totalUnggulan/countUnggulan;
-                System.out.printf("Unggulan : rata-rata IPK = %.2f\n", rataUnggulan);
-            }else {
-                System.out.println("Unggulan : tidak ada pendaftar");
-            }
-
-            if (countRiset > 0) {
-                double rataRiset = totalRiset/countRiset;
-                System.out.printf("Riset    : rata-rata IPK = %.2f\n", rataRiset);
-            }else {
-                System.out.println("Riset    : tidak ada pendaftar");
-            }
-            System.out.println();
+        if (countReguler > 0){
+            double rataReguler = totalReguler/countReguler;
+            System.out.printf("Reguler  : rata-rata IPK = %.2f\n", rataReguler);
+        }else {
+            System.out.println("Reguler  : tidak ada pendaftar");
         }
+
+        if (countUnggulan > 0) {
+            double rataUnggulan = totalUnggulan/countUnggulan;
+            System.out.printf("Unggulan : rata-rata IPK = %.2f\n", rataUnggulan);
+        }else {
+            System.out.println("Unggulan : tidak ada pendaftar");
+        }
+
+        if (countRiset > 0) {
+            double rataRiset = totalRiset/countRiset;
+            System.out.printf("Riset    : rata-rata IPK = %.2f\n", rataRiset);
+        }else {
+            System.out.println("Riset    : tidak ada pendaftar");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int pilihan;
 
